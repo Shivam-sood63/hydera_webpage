a7e8b981f3c3abc34cb5e0cb75c1334705ae8ec0
Shivam sood <155950691+Shivam-sood63@users.noreply.github.com>
2025-11-06 22:21:51 +0530

    new update

--- src/Components/Conseption.jsx
+++ src/Components/Conseption.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
-import ellisp from "../assets/Ellipse1.png"
+// import ellisp from "../assets/Ellipse1.png"
 
 const Conseption = () => {
   return (
     <div className='w-full flex items-center justify-center bg-black text-white'>
       <div className='lg:w-[80%] lg:flex items-center justify-center gap-12 p-5'>
         <div className='my-5 text-center md:my-5'>
-            <div className="bg-[url('src/assets/Ellipse1.png')] bg-cover bg-center lg:h-42 md:h-42 rounded-full">
+            <div className="bg-[url('public/Ellipse1.png')] bg-cover bg-center lg:h-42 md:h-42 rounded-full">
             <h1 className='md:text-9xl lg:text-9xl p-4 text-black font-bold'>1</h1>
             </div>
             <h1 className=''>3D Conception & Design</h1>
         </div>
          <div className='my-5 text-center md:my-5'>
-            <div className="bg-[url('src/assets/Ellipse1.png')] bg-cover bg-center lg:h-42 md:h-42 rounded-full">
+            <div className="bg-[url('public/Ellipse1.png')] bg-cover bg-center lg:h-42 md:h-42 rounded-full">
             <h1 className='md:text-9xl lg:text-9xl p-4 text-black font-bold'>2</h1>
             </div>
             <h1 className=''>3D Conception & Design</h1>
         </div>
          <div className='my-5 text-center md:my-5'>
-            <div className="bg-[url('src/assets/Ellipse1.png')] bg-cover bg-center md:h-42 lg:h-42 rounded-full">
+            <div className="bg-[url('public/Ellipse1.png')] bg-cover bg-center md:h-42 lg:h-42 rounded-full">
             <h1 className='md:text-9xl lg:text-9xl p-4 text-black font-bold'> 3</h1>
             </div>
             <h1 className=''>3D Conception & Design</h1>
         </div>
           <div className='my-5 text-center md:my-5 '>
-            <div className="bg-[url('src/assets/Ellipse1.png')] bg-cover bg-center md:h-42 lg:h-42 rounded-full">
+            <div className="bg-[url('public/Ellipse1.png')] bg-cover bg-center md:h-42 lg:h-42 rounded-full">
             <h1 className='md:text-9xl lg:text-9xl p-4 text-black font-bold'>4</h1>
             </div>
             <h1 className=''>3D Conception & Design</h1>
